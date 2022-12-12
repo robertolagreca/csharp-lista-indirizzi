71e1e9332905b2717af09f3b8cd0b5c38fe255f4
robertolagreca <robertolagreca92@gmail.com>
2022-12-12 15:15:54 +0100

    Struttura base

---
+++ ListaIndirizzi/DataUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class DataUser
+    {
+        private string _name, _surname, _address, _city, _province, _zip;
+
+        public DataUser(string name, string surname, string address, string city, string province, string zip)
+        {
+            _name = name;
+            _surname = surname;
+            _address = address;
+            _city = city;
+            _province = province;
+            _zip = zip;
+        }
+
+        public string GetName() { return _name; }
+        public string GetSurname() { return _surname; }
+        public string GetAddress() { return _address; }
+        public string GetCity() { return _city; }
+        public string GetProvince() { return _province; }
+        public string GetZip() { return _zip; }
+
+        public void SetName(string name) { _name = name; }
+        public void SetSurname(string surname) { _surname = surname; }
+        public void SetAddress(string address) { _address = address; }
+        public void SetCity(string city) { _city = city; }
+        public void SetProvince(string province) { _province = province; }
+        public void SetZip(string zip) { _zip = zip; }
+    }
+}
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in
+//classe, e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a
+//partire dalla classe Indirizzo.
+
+//Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo
+//genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+
+StreamReader file;
+string line;
+
+
+
+file = File.OpenText("C:\\Users\\robsm\\Documents\\Generation Coding\\Dotnet\\ListaIndirizzi\\ListaIndirizzi\\File\\addresses.csv");
+
+while (!file.EndOfStream)
+{
+    line = file.ReadLine();
+
+
+    string[] singleData = line.Split(", ");
+
+
+}
+
+file.Close();
